1aa71678ff24e69295972f6aa9c22dbc56aedce9
Minsu <karosis28@gmail.com>
2015-04-22 16:29:21 +0900

    Define funtion SWAP, selection_sort

---
+++ Selection_Sort/Selection_Sort/selection_sort.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define SWAP(x, y, t) ( (t)=(x), (x)=(y), (y)=(t) )
+
+void selection_sort(int list[], int n);
+
+int main()
+{
+	return 0;
+}
+
+void selection_sort(int list[], int n)
+{
+
+}
