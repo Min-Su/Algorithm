a80385423e00f5058487930503e899ff128cdf54
Minsu <karosis28@gmail.com>
2015-04-23 22:34:25 +0900

    Shell_sort sorting test

--- Shell_Sort/Shell_Sort/shell_sort.cpp
+++ Shell_Sort/Shell_Sort/shell_sort.cpp
@@ -1,12 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define MAX_LIST_SIZE 100
+
 void inc_insertion_sort(int list[], int first, int last, int gap);
 void shell_sort(int list[], int n);
 
 int main()
 {
+	int i;
+	int list[MAX_LIST_SIZE];
 
+	for(i = 0; i < MAX_LIST_SIZE; i++) {
+		list[i] = rand()%100;
+		printf("%d ", list[i]);
+	}
+
+	printf("\n------------------------------------Sort------------------------------------\n");
+	shell_sort(list, MAX_LIST_SIZE);
+
+	for(i = 0; i < MAX_LIST_SIZE; i++) {
+		printf("%d ", list[i]);
+	}
+	printf("\n");
 }
 
 void inc_insertion_sort(int list[], int first, int last, int gap)
