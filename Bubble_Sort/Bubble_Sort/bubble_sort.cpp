ac0d5abd0e7e87bd68146168ef39600b4c02bde1
Minsu <karosis28@gmail.com>
2015-04-23 15:03:33 +0900

    bubble_sort sorting test

--- Bubble_Sort/Bubble_Sort/bubble_sort.cpp
+++ Bubble_Sort/Bubble_Sort/bubble_sort.cpp
@@ -1,12 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define MAX_LIST_SIZE 100
 #define SWAP(x, y, t) ( (t)=(x), (x)=(y), (y)=(t) )
 
 void bubble_sort(int list[], int n); 
 
 int main()
 {
+	int list[MAX_LIST_SIZE];
+	int i;
+
+	for(i = 0; i < MAX_LIST_SIZE; i++) {
+		list[i] = rand()%100;
+		printf("%d ", list[i]);
+	}
+
+	bubble_sort(list, MAX_LIST_SIZE);
+	printf("\n------------------SORT-----------------\n");
+
+	for(i = 0; i < MAX_LIST_SIZE; i++) {
+		printf("%d ", list[i]);
+	}
 
 	return 0;
 }
