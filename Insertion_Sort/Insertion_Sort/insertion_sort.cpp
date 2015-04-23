bb43b5b9cb535ec9e64c5eb7bd69fcdd03ba7121
Minsu <karosis28@gmail.com>
2015-04-23 10:36:56 +0900

    Insertion_sort testing 0 ~ 99 rand() sorting

--- Insertion_Sort/Insertion_Sort/insertion_sort.cpp
+++ Insertion_Sort/Insertion_Sort/insertion_sort.cpp
@@ -1,12 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define MAX_SIZE 1000
+#define MAX_SIZE 100
 
 void insertion_sort(int list[], int n);
 
 int main()
 {
+	int list[MAX_SIZE];
+	int i;
+
+	for(i = 0; i < MAX_SIZE; i++) {
+		list[i] = rand()%MAX_SIZE;
+	}
+	
+	insertion_sort(list, MAX_SIZE);
+
+	for(i = 0; i < MAX_SIZE; i++) {
+		printf("%d ", list[i]);
+	}
+
 	return 0;
 }
 
