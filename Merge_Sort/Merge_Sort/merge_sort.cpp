16f69bef467e9272b60237bcda477f283a5c6870
Minsu <karosis28@gmail.com>
2015-04-27 16:36:39 +0900

    Merge_sort funtion, merge funtion add

--- Merge_Sort/Merge_Sort/merge_sort.cpp
+++ Merge_Sort/Merge_Sort/merge_sort.cpp
@@ -1,8 +1,24 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define MAX_SIZE 100
+
+int sorted[MAX_SIZE];
+
+void merge_sort(int list[], int left, int right);
+void merge(int list[], int left, int mid, int right);
 
 int main()
 {
 	return 0;
+}
+
+void merge_sort(int list[], int left, int right)
+{
+
+}
+
+void merge(int list[], int left, int mid, int right)
+{
+
 }
