51149563c8101400b598e76f552f10f05d7d662f
Minsu <karosis28@gmail.com>
2015-04-27 23:55:56 +0900

    Definition of funtion : quick_sort, partition

--- Quick_Sort/Quick_Sort/quick_sort.cpp
+++ Quick_Sort/Quick_Sort/quick_sort.cpp
@@ -1,8 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+void quick_sort(int list[], int left, int right);
+int partition(int list[], int left, int right);
 
 int main()
 {
 	return 0;
+}
+
+void quick_sort(int list[], int left, int right)
+{
+
+}
+
+int partition(int list[], int left, int right)
+{
+
 }
